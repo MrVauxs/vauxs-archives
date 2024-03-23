468a0c8867188b712d078fd28e8c264d46dc4f3c
mrvauxs <mrvauxs@gmail.com>
2024-03-23 13:02:38 +0000

    Add explicit warning #3

--- src/index.js
+++ src/index.js
@@ -11,3 +11,12 @@ Hooks.once("init", () => {
 	VArchChatLogHook();
 });
 
+Hooks.once("ready", () => {
+	if (game.modules.get("df-chat-enhance")?.active) {
+		ui.notifications.warn(
+			"DF Chat Enhancements is outdated and not compatible with Vauxs' Archives, causing issues when browsing archives. Please disable DF Chat Enhancements.",
+			{ permanent: true }
+		);
+	}
+});
+
