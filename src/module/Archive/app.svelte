4c287d25a6a7a629e3ac921825f370bc87363368
mrvauxs <mrvauxs@gmail.com>
2025-07-27 19:54:23 +0200

    oh god help me

---
+++ src/module/Archive/app.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<main>
+	oh god this is a lot worse now
+</main>
+
+<style lang="scss">
+	main {
+		max-height: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: row;
+		gap: 0.125rem;
+	}
+</style>
